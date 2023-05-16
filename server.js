d64e789ddef420c345b91dc84a8be7840a8da46a
STANdaard05 <stanvogels158@gmail.com>
2023-05-16 14:12:44 +0200

    css is gefixed

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
+const express = require('express')
 const db = require('./utils/Database.js')
+const app = express()
 const dotenv = require("dotenv")
-const express = require('express');
-const app = express();
-
 dotenv.config()
 db.connect()
 
@@ -11,6 +10,8 @@ app.set('view engine', 'ejs')
 app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+
+app.use(express.static(__dirname + '../../public'));
 
 
 app.use("/account", require("./routes/account"))
