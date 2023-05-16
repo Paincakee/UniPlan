7d09922cd9e92f0aeb75bfeb941ae8599b14b5c9
Fabiwell <80628214+Fabiwell@users.noreply.github.com>
2023-05-16 10:04:23 +0200

    Update csv.js

--- routes/csv.js
+++ routes/csv.js
@@ -1,44 +1,23 @@
-const express = require('express')
-const app = express()
-const router = express.Router()
-const csv = require('csv-parser')
-const multer = require('multer');
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-// Add a middleware to set the 'Content-Type' header
-app.use((req, res, next) => {
-  req.headers['content-type'] = 'multipart/form-data';
-  next();
-})
+const express = require('express');
+const app = express();
+const router = express.Router();
+var bodyParser = require("body-parser");
+var multer  = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
 
 
 router.get("/", (req, res) => {
-    res.render("csv/import")
-  })
-  
-  router.post("/", (req, res) => {
-    // console.log('success')
-    // res.render("csv/import")
-  
-    // if (!req.file) {
-    //   res.status(400).send('No file uploaded.');
-    //   return;
-    // }
-  
-    // Access the CSV data in req.file.buffer
-    const csvData = req.file.buffer.toString();
-  
-    // Process the CSV data using csv-parser
-    const results = [];
-    csv.parse(csvData)
-      .on('data', (data) => results.push(data))
-      .on('end', () => {
-        // Do something with the parsed CSV data
-        console.log(results);
-        res.send('File uploaded and processed successfully.');
-      });
-  })
-  
-  module.exports = router;
+  res.render("csv/import");
+});
+
+router.post("/", upload.single('csv'), (req, res) => {
+  var b = req.file["buffer"]
+  console.log(b.toString())
+  res.send(b.toString())
+});
+
+module.exports = router;
