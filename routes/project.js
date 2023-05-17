549e51e7a006f37869e58849b33042139fe17a26
Fabiwell <80628214+Fabiwell@users.noreply.github.com>
2023-05-17 14:43:15 +0200

    update

--- routes/project.js
+++ routes/project.js
@@ -1,36 +1,35 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const router = express.Router();
+
+const upload = multer({ dest: __dirname + '/../resources/upload/' });
+
+router.post('/new', upload.array(['files', 'fotos']), async (req, res) => {
+  // Access the uploaded files via req.files
+  // Iterate over the files and save them to a folder
+  req.files.forEach((file) => {
+    // Save the file to a specific folder using the file.originalname property
+    fs.renameSync(file.path, __dirname + '/../resources/upload/' + req.session.email + '/' + req.body.title + '/' + file.originalname);
+  });
+
+  const result = await db.sql("project/createProject", {
+    table: "projects",
+    user_id: req.session.email,
+    title: req.body.title,
+    description: req.body.description,
+    contact_info: req.body.contact_info
+  })
+
+  res.render('project/home');
+});
 
 router.get('/', function(req, res) {
-    res.render('project/home')
+  res.render('project/home');
 });
 
 router.get('/new', (req, res) => {
-    res.render('project/create')
-})
+  res.render('project/create');
+});
 
-router.post('/new', (req, res) => {
-    res.render('project/home')
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = router
+module.exports = router;
