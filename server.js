12a4dca240232de52002cfe0ffbbdf4208884bd2
Richard Vullings <rvullings8@gmail.com>
2023-05-17 14:34:56 +0200

    Working chat (No chat save)

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
-const db = require('./utils/Database.js')
-const nodemailer = require("nodemailer");
 const app = express()
+const db = require('./utils/Database.js')
+const nodemailer = require("nodemailer")
 const session = require('express-session')
 const dotenv = require("dotenv")
+const http = require('http');
+const socketIO = require('socket.io')
+const { log } = require('console')
+
+const server = http.createServer(app);
+const io = socketIO(server);
+
 dotenv.config()
 db.connect()
 
@@ -15,14 +22,15 @@ app.use(session({
 
 app.set('view engine', 'ejs')
 
+app.use(express.static(__dirname));
 app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-app.use(express.static(__dirname + '../../public'));
 app.use("/csv", require("./routes/csv.js"))
 app.use("/account", require("./routes/account"))
 app.use("/project", require("./routes/project.js"))
+app.use("/chat", require("./routes/chat.js"))
 
 
 let transporter = nodemailer.createTransport({
@@ -35,8 +43,26 @@ let transporter = nodemailer.createTransport({
     },
 });
 
-app.listen(3000)
+const users = {}
 
+io.on('connection', (socket) => {
+
+  // socket.emit('chat-message', 'bozo')
+  socket.on('new-user', user => {
+    users[socket.id] = user
+    console.log(user);
+  })
+  socket.on('send-chat-message', message => {
+
+    socket.broadcast.emit('chat-message', {message: message, user: users[socket.id]})
+  })
+
+  
+
+});
+
+const port = 3000;
+server.listen(port);
 
 global.db = db
 global.sender = transporter
