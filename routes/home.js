7609dd0842d2a8c011d1792d0bce5d78f51abd06
Richard Vullings <rvullings8@gmail.com>
2023-06-28 10:16:17 +0200

    CheckLoggedIn (home.js)

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', async (req, res) => {
+app.get('/', checkLoggedIn, async (req, res) => {
   try {
     // Add your code here to handle the request and send a response
     res.render("home/dashboard", {
@@ -13,5 +13,20 @@ app.get('/', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+// Helper Functions
+
+//This function checks if the user is logged in, if thats not the case go to login page
+function checkLoggedIn(req, res, next) {
+    // Check if the email session is set and not null
+    if (!req.session.email || !req.session.loggedIn) {
+      // Redirect the user to a specific route if they are not logged in
+      res.redirect('/account/login');
+    }
+    else {
+      // If the user is logged in,Move to the next middleware/route handler
+      next();
+    }
+  }
 
 module.exports = app;
